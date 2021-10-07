38f13703b462f79a8cd28a7a9cb342dcf28da4ac
MasonOrr <68841584+MasonOrr@users.noreply.github.com>
2021-10-07 00:11:44 -0500

    Refactored the functions for user response choosing search mode to better fit a OOP organization model.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from sys import exit
+from typing import List
+from response import ResponseHandler
 
 
 def main():
@@ -13,41 +15,12 @@ Pokemon search allows you to search for the pokemon within a particular route.
 
 
 def attain_search_mode():
-    """This function allows the user to enter what search mode they
-    would like to use, ask for help, and to quit the program when finished
+    search_mode = None
+    response_handler = ResponseHandler()
 
-	Takes user input on mode and compares it to a list of possible inputs.
-	Then it calls the desired search function, returns a help string, or quits the program
-	"""
-
-    mode_response_list = ['rs', 'route search', 'ps', 'pokemon search', 'quit', 'help']
-
-    while True:
+    while search_mode is None:
         mode_response = input('''Would you like to use Route Search(RS), Pokemon Search(PS), (quit), or (help)?''')
-
-        if mode_response.lower() not in mode_response_list:
-            print("Please input a valid response. Valid responses are: ",
-                  f"{', '.join(str(response) for response in mode_response_list)}")
-
-        elif mode_response.lower() == 'quit':
-            print('Thank you for using Pokemon Route Search.')
-            exit()
-        elif mode_response.lower() == 'help':
-            print('''
-Valid responses are rs, ps, quit, and help. 
-RS or route search returns all routes that a pokemon appears in. 
-PS or pokemon search returns all pokemon that appear within a particular route.
-Quit exits the program.
-''')
-
-        else:
-            search_mode = mode_response.lower()
-            break
-    if search_mode == 'rs' or 'route_search':
-        route_search()
-
-    elif search_mode == 'ps' or 'pokemon search':
-        pokemon_search()
+        response_handler.search_mode_handler(mode_response)
 
 
 def route_search():  # TODO: retrieve/validate data via api wrapper
