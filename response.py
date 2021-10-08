99cfe378a1046d022ff686f3bfeb5df77cdc40e3
MasonOrr <68841584+MasonOrr@users.noreply.github.com>
2021-10-08 12:48:36 -0500

    Further refactoring to better encapsulate the ResponseHandler and SearchType classes.

--- response.py
+++ response.py
@@ -1,34 +1,67 @@
+from sys import exit
+from textwrap import dedent
+
+
 class ResponseHandler:
-    def search_mode_handler(self, response):
-        """Interprets users response and returns selected mode"""
+    def __init__(self):
+        self.input_prompt = ''
+        self.response = ''
+        self.valid_responses = []
+        self.valid_search = False
+        self.help_text = ''
+        self.context = ''
+        return
 
-        search_mode = None
-        allowed_responses = ['rs', 'route search', 'ps', 'pokemon search', 'quit', 'help']
+    def get_response(self):
+        """User input prompt based on input_prompt attr and responds to help. lower() to sanitize input"""
 
-        if response.lower() not in allowed_responses:
+        self.response = input(f'{self.input_prompt}').lower()
+
+        if self.response == 'help':
+            print(dedent(self.help_text))
+            self.get_response()
+        return
+
+    def validate_response(self):
+        """Checks if user input is in valid_responses attr and informs user of valid options if not. Also quits"""
+        if self.response in self.valid_responses:
+            self.valid_search = True
+            return
+        elif self.response == 'quit':
+            return exit()
+        else:
             print("Please input a valid response. Valid responses are: ",
-                  f"{', '.join(str(response) for response in allowed_responses)}")
+                  f"{', '.join(item for item in self.valid_responses)} \n")
 
-        elif response.lower() == 'quit':
-            print('Thank you for using Pokemon Route Search.')
-            exit()
 
-        elif response.lower() == 'help':
-            print('''
-Valid responses are rs, ps, quit, and help. 
-RS or route search returns all routes that a pokemon appears in. 
-PS or pokemon search returns all pokemon that appear within a particular route.
-Quit exits the program.
-''')
+class SearchType(ResponseHandler):
+    def __init__(self):
+        super().__init__()
 
-        elif response.lower() == 'rs' or 'route_search':
-            search_mode = "route_search"
+        self.input_prompt = "Would you like to use Route Search(RS), Pokemon Search(PS), (quit), or (help)?"
+        self.valid_responses = ['rs', 'route search', 'ps', 'pokemon search', 'quit', 'help']
+        self.help_text = '''\
+                            Valid responses are rs, ps, quit, and help. 
+                            RS or route search returns all routes that a pokemon appears in. 
+                            PS or pokemon search returns all pokemon that appear within a particular route.
+                            Quit exits the program.
+                        '''
 
-        elif response.lower() == 'ps' or 'pokemon search':
-            search_mode = "pokemon_search"
+    def get_search_type(self):
+        """Returns search type class based on user input. Added exception in case of bad validation"""
+        if self.response == 'rs' or 'route search':
+            return RouteSearch()
+        elif self.response == 'ps' or 'pokemon search':
+            return PokemonSearch()
+        else:
+            raise Exception('Unexpected search type in self.response')
 
-    def pokemon_search_handler(self):
-        return
 
-    def route_search_handler(self):
-        return
+class PokemonSearch(ResponseHandler):
+    def __init__(self):
+        super().__init__()
+
+
+class RouteSearch(ResponseHandler):
+    def __init__(self):
+        super().__init__()
